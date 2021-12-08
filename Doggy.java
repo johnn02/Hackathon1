9cbb48df05c6687402d338dd0aeed35b59c3bf2c
Pagronpu <95639907+Pagronpu@users.noreply.github.com>
2021-12-08 16:31:34 +0800

    Add files via upload

---
+++ Doggy.java
@@ -0,0 +1,38 @@
+/*public class Doggy {
+    public static void main(String[] args) {
+        //int ar[] = {1, 3, 1};// true
+        //int ar[] = {1, 3, 3, 1};// true
+        //int ar[] = {2, 1, 3, 1};// false
+        //int ar[] = {2, 1, 3, 1, 2};// true
+        //int ar[] = {2}; // true
+        //int ar[] = {2,1}; // false
+
+        int i=0;
+        int n=ar.length;
+        int counter=0;
+        boolean isPalindrome=true;
+
+        if (n==1){
+            System.out.println(isPalindrome=true);
+        }
+        else if (n==2 || n==3){
+            if(ar[i]==ar[n-1]){
+                System.out.println(isPalindrome=true);
+            }
+            else{
+                System.out.println(isPalindrome=false);
+            }
+        }
+        else if (n==4 || n==5){
+            if ((ar[i]==ar[n-1]) && (ar[i+1]==ar[n-2])){
+                System.out.println(isPalindrome=true);
+            }
+            else{
+                System.out.println(isPalindrome=false);
+            }
+        }
+
+
+
+    }
+} */
